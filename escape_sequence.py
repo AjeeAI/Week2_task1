d7ccad087fc3843901851d38fb4aaf50f7006696
Ajijolaoluwa Adesoji <ajeeaidev@gmail.com>
2025-08-08 14:06:06 +0100

    First push

---
+++ escape_sequence.py
@@ -0,0 +1,33 @@
+# Escape sequences in print - "\", "\t"
+print("Line1\nLine2") #New line
+print("Iem1\nItem2") # Tab space
+print("He said, \"Hello\"") # Quoted string
+print('It\'s Python!') # Apostrophe in single quote
+
+# Newline (\n)
+print("Welcome to Python\nLet's learn together!")
+
+# Tab (\t)
+print("Name\tAge\tLocation")
+print("Aisha\t25\tLagos")
+
+#Qoutes inside string 
+
+print("She said, \"Hello there!\"")
+print('It\'s a sunny day.')
+
+#Backslash
+
+print("\t\t\tT\t\t\t")
+
+print("File path: C:\\Users\\Aisha\\Documents")
+
+# print("\t\t\t*\t\t\t\n\t\t*\t\t*\n\t*\t*\t*\t*\t*")
+
+# print("\t\t\t*\t\t\t\n\t\t*\t\t*\n\t*\t*\t*\t*\t*\n*\t*\t")
+
+
+
+print("\t *\t\n\t* *\n       * * *\n      * * * *\n     * * * * *\n    * * * * * *\n   * * * * * * *\n  * * * * * * * *\n * * * * * * * * *\n* * * * * * * * * * ")
+print("\n")
+print("\t\t*\n\t\t* *")
