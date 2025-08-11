add133e1d1407a6dd611885e5ccb7405bc3997f7
Ajijolaoluwa Adesoji <ajeeaidev@gmail.com>
2025-08-11 15:28:05 +0100

    First task for week one

---
+++ Week2_task3.py
@@ -0,0 +1,57 @@
+# Question 1
+# entry = input("Enter info here: ")
+# print(entry.upper())
+
+# # Question2
+# entry = "python"
+# print(f"The first character is {entry[0]} and the last character is {entry[-1]}")
+
+# # Question 3
+# name = input("Enter your name: ")
+# print(name.replace(name, "Hello, !"))
+
+# # Question 4
+# #Thus is a function supposedly to get the length without using the len() function
+
+# entry = "Adesoji"
+
+# length = entry.find(entry[-1])  + 1
+# # print(len(entry))
+# print(F"The length of the entry is {length}")
+
+
+# Task 5
+
+# message = "Hello World"
+# print(message.replace("World", "Python"))
+"""
+Task 2
+"""
+
+# Question 6
+# entry = " This is a java file for checking"
+# print("python" in entry)
+
+# Question 7
+entry = "Ajijolaoluwa"
+print(entry[-1::-1])
+
+
+# Question 8
+entry = " This is my name     "
+
+print(entry.strip())
+
+# Question 9
+
+# entry = input("Enter a sentence: ")
+
+# Question 10
+entry = "1234"
+int_entry = int(entry)
+print(int_entry * 2)
+
+""" Task 3"""
+sentence = input("Enter a sentence: ")
+word = sentence.strip("\n")
+print(word)
